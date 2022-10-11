37d52ba4aaef22c19acfc7d4adb177c2ffd98b03
gulberkdemir <62015829+gulberkdemir@users.noreply.github.com>
2022-10-12 01:30:13 +0300

    tests were added

--- apps/lastname-e2e/src/e2e/app.cy.ts
+++ apps/lastname-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,35 @@
 import { getGreeting } from '../support/app.po';
+import {movieApiConfig} from "../../../lastname/src/app/movie-config";
 
 describe('lastname', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome lastname');
+
+  it('First title should be now playing', () => {
+
+    cy.visit('http://localhost:4200/')
+    cy.get('h1').first().contains('Now Playing')
+
   });
+  it('Second title should be now playing', () => {
+
+    cy.get('h1').last().contains('Popular Movies')
+
+
+  });
+
+  it('When Titanic is typed on search, exact/same movie should be found', () => {
+    cy.visit('http://localhost:4200/movie-search');
+    cy.get('input').type('titanic')
+    cy.get('.mat-button-wrapper', { timeout: 10000 }).should('be.visible');
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000);
+    cy.get('body > last-root > last-movie-search-page > div > section > last-result-tile > div > mat-card:nth-child(1) > mat-grid-list > div > mat-grid-tile:nth-child(2) > div > mat-card-content > mat-card-header > div.mat-card-header-text > mat-card-title > a > span.mat-button-wrapper').contains('Titanic');
+
+
+
+  });
+
+
 });
